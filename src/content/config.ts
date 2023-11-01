53be31d0822a58ead4b2409764abdc1d2cc2848e
Isaac Kwok <kch062522@gmail.com>
2023-11-01 21:28:25 +0800

    fix: image problem

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: image(),
-  }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		archived: z.boolean().optional()
+	}),
 });
 
 export const collections = { blog };
