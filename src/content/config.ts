b7fec0fb3186591343066f10409feac3c816244c
Isaac Kwok <kch062522@gmail.com>
2023-11-01 21:22:01 +0800

    fix: image typing

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
-const ZImageMetaData: z.ZodType<ImageMetadata> = z.object({
-  src: z.string(),
-  width: z.number(),
-  height: z.number(),
-	format: z.enum(["jpeg","jpg","png","tiff","webp","gif","svg","avif"])
-});
-
 const blog = defineCollection({
   // Type-check frontmatter using a schema
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: ZImageMetaData.or(z.string()).optional(),
-    archived: z.boolean().optional(),
+    heroImage: image(),
   }),
 });
 
